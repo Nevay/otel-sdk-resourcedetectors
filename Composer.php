79e9cf656ca4b58fa785bcbd55324c22ef37f3bf
Tobias Bachert <git@b-privat.de>
2024-05-05 20:59:34 +0200

    Remove `class_exists` check for `InstalledVersions`

--- Composer.php
+++ Composer.php
@@ -5,7 +5,6 @@ use Composer\InstalledVersions;
 use Nevay\OTelSDK\Common\Attributes;
 use Nevay\OTelSDK\Common\Resource;
 use Nevay\OTelSDK\Common\ResourceDetector;
-use function class_exists;
 
 /**
  * @see https://opentelemetry.io/docs/specs/semconv/resource/#service
@@ -14,16 +13,14 @@ final class Composer implements ResourceDetector {
 
     public function getResource(): Resource {
         $composer = [];
-        if (class_exists(InstalledVersions::class)) {
-            $composer['service.name'] = InstalledVersions::getRootPackage()['name'];
-            $composer['service.version'] = InstalledVersions::getRootPackage()['pretty_version'];
+        $composer['service.name'] = InstalledVersions::getRootPackage()['name'];
+        $composer['service.version'] = InstalledVersions::getRootPackage()['pretty_version'];
 
-            if ($composer['service.name'] === '__root__') {
-                unset($composer['service.name']);
-            }
-            if ($composer['service.version'] === '1.0.0+no-version-set') {
-                unset($composer['service.version']);
-            }
+        if ($composer['service.name'] === '__root__') {
+            unset($composer['service.name']);
+        }
+        if ($composer['service.version'] === '1.0.0+no-version-set') {
+            unset($composer['service.version']);
         }
 
         return new Resource(
