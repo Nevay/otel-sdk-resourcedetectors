025d29cddc6e8690969b734778011ee7b2b14e2c
Tobias Bachert <git@b-privat.de>
2025-03-21 23:39:48 +0100

    Do not read from `/proc/self/cmdline` if it was modified via `cli_set_process_title()`

--- Process.php
+++ Process.php
@@ -6,9 +6,11 @@ use Nevay\OTelSDK\Common\Resource;
 use Nevay\OTelSDK\Common\ResourceDetector;
 use function array_pop;
 use function basename;
+use function cli_get_process_title;
 use function explode;
 use function extension_loaded;
 use function file_get_contents;
+use function function_exists;
 use function getmypid;
 use function restore_error_handler;
 use function set_error_handler;
@@ -28,7 +30,7 @@ final class Process implements ResourceDetector {
         }
 
         $commandLine = self::commandLine();
-        $process['process.executable.name'] = basename($commandLine[0]);
+        $process['process.executable.name'] = basename(PHP_BINARY);
         $process['process.executable.path'] = PHP_BINARY;
         $process['process.command'] = $commandLine[0];
         $process['process.command_args'] = $commandLine;
@@ -44,21 +46,29 @@ final class Process implements ResourceDetector {
     }
 
     private static function commandLine(): array {
-        if (PHP_OS_FAMILY === 'Linux') {
-            set_error_handler(static fn() => null);
-            try {
-                if (($content = file_get_contents('/proc/self/cmdline')) !== false) {
-                    $parsed = explode("\0", $content);
-                    array_pop($parsed);
-                    if ($parsed) {
-                        return $parsed;
-                    }
+        if (PHP_OS_FAMILY === 'Linux' && (!function_exists('cli_get_process_title') || @cli_get_process_title() === '')) {
+            if (($content = self::read('/proc/self/cmdline')) !== null) {
+                $parsed = explode("\0", $content);
+                array_pop($parsed);
+                if ($parsed) {
+                    return $parsed;
                 }
-            } finally {
-                restore_error_handler();
             }
         }
 
-        return [PHP_BINARY, ...$_SERVER['argv']];
+        return [PHP_BINARY, ...$_SERVER['argv'] ?? []];
+    }
+
+    private static function read(string $file): ?string {
+        set_error_handler(static fn() => null);
+        try {
+            if (($content = file_get_contents($file)) !== false) {
+                return $content;
+            }
+        } finally {
+            restore_error_handler();
+        }
+
+        return null;
     }
 }
