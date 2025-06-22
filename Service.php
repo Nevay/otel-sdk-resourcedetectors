dfcdfbdc201f06e7f286f031e8fb75e4681f0507
Tobias Bachert <git@b-privat.de>
2025-06-22 21:16:49 +0200

    Add `service` detector

---
+++ Service.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+namespace Nevay\OTelSDK\Common\ResourceDetector;
+
+use Nevay\OTelSDK\Common\Attributes;
+use Nevay\OTelSDK\Common\Resource;
+use Nevay\OTelSDK\Common\ResourceDetector;
+
+final class Service implements ResourceDetector {
+
+    public function getResource(): Resource {
+        $service = [];
+        if (($serviceName = $_SERVER['OTEL_SERVICE_NAME'] ?? null) !== null) {
+            $service['service.name'] = $serviceName;
+        }
+
+        return new Resource(
+            new Attributes($service),
+            schemaUrl: 'https://opentelemetry.io/schemas/1.25.0',
+        );
+    }
+}
